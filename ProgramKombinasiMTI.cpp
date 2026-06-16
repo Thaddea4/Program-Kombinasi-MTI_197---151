c4c501313fcc4cf84d553e1fd2da4a9d6cdd530d
Thaddea4 <adeyasyie@gmail.com>
2026-06-16 14:09:03 +0700

    menginput password

--- ProgramKombinasiMTI.cpp
+++ ProgramKombinasiMTI.cpp
@@ -6,5 +6,10 @@
 using namespace std;
 
 int main() {
-    
+    cout.setf(ios::fixed);
+
+    string password;
+    cout << "=== ANALISIS KEAMANAN PASSWORD ===" << endl;
+    cout << "Masukkan password yang ingin diuji: ";
+    getline(cin, password);
 }
